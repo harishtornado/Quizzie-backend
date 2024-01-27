cc7fb9d509c18152835caca3d69057fbe8cb3a2c
harish <77318230+harishtornado@users.noreply.github.com>
2024-01-27 10:02:10 +0530

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,17 @@
 import User from "../models/UserModal.js";
+import bcrypt from "bcrypt";
 
 export const fetchUser = async (req, res) => {
   try {
     const Users = await User.find();
-    res
-      .status(200)
-      .json({ message: "users fetched", data: Users, status: "ok" });
+    if (Users) {
+      res
+        .status(200)
+        .json({ message: "users fetched", data: Users, status: "ok" });
+    }
   } catch (err) {
     res
-      .status(404)
+      .status(500)
       .json({ message: "error fetching user", error: err.message });
   }
 };
@@ -17,13 +20,14 @@ export const loginUser = async (req, res) => {
   const { email, password } = await req.body;
   try {
     const user = await User.findOne({ email: email });
+    const passwordMatch = await bcrypt.compare(password, user.password);
     if (user) {
-      if (user.password === password) {
+      if (passwordMatch) {
         return res
           .status(200)
           .json({ message: "User found", data: user, status: "ok" });
       } else {
-        return res.status(404).json({
+        return res.status(401).json({
           message: "Password Incorrect",
           data: null,
           status: "failed",
@@ -34,7 +38,7 @@ export const loginUser = async (req, res) => {
       .status(404)
       .json({ message: "No such User", data: null, status: "failed" });
   } catch (err) {
-    res.status(404).json({
+    res.status(500).json({
       message: "Error Logging in",
       error: err.message,
       status: "failed",
@@ -43,23 +47,27 @@ export const loginUser = async (req, res) => {
 };
 
 export const newUser = async (req, res) => {
-  const user = await req.body;
+  const { userName, email, password } = req.body;
+
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
-      userName: user.userName,
-      email: user.email,
-      password: user.password,
+      userName,
+      email,
+      password: hashedPassword,
     });
 
     await newUser.save();
+
     res.status(201).json({
       message: "User created successfully",
       data: newUser,
       status: "ok",
     });
   } catch (err) {
-    res.status(404).json({
-      message: "Error Logging in",
+    res.status(500).json({
+      message: "Error creating user",
       error: err.message,
       status: "failed",
     });
